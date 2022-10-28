0a1c7ed92ff406e200d7d8b2df44f8f7ffcd884f
Chic玖歌 <93077394+ChicJiuGe@users.noreply.github.com>
2022-10-28 20:05:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1YDL55pm9F7ThGY4hGg9uaBpagg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'phrcJuwfIFaRFDEETHxk1DDb4_kNno4fto3hpHyDEpM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-16',
       festivals: [

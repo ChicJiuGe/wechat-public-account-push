ac05045f00edbf62a135a9af3c1155b1f0e271c5
Chic玖歌 <93077394+ChicJiuGe@users.noreply.github.com>
2022-10-28 20:45:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,7 +19,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '乖乖',
+      name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o1YDL55pm9F7ThGY4hGg9uaBpagg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID

eab1407e9d66b6082b371c37fff98f08f56fe85b
Chic玖歌 <93077394+ChicJiuGe@users.noreply.github.com>
2022-10-28 19:39:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'OboKkGjWlJXqDIvP1GfpLxnX4LR4rkUROiu_2hZve2c',
+  CALLBACK_TEMPLATE_ID: 'JqAEY8CFkhlzq1kfOTLD941SP_pbv7IUTlGAANvxQ1k',
 
   CALLBACK_USERS: [
     {
